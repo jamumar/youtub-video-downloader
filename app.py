2410184e5bd059593be0d5bd264484f209eb5d04
jamumar <88538044+jamumar@users.noreply.github.com>
2024-06-05 09:04:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask, render_template, request, jsonify, send_from_directory
+from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 import yt_dlp
 import threading
 import re
+from pathlib import Path
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -12,13 +13,7 @@ def strip_ansi_escape_sequences(text):
     ansi_escape = re.compile(r'\x1b\[[0-9;]*m')
     return ansi_escape.sub('', text)
 
-def get_downloads_folder():
-    if os.name == 'nt':  # For Windows
-        return os.path.join(os.environ['USERPROFILE'], 'Downloads')
-    else:  # For macOS and Linux
-        return os.path.join(os.path.expanduser('~'), 'Downloads')
-
-def download_video(url, resolution, socketio=None):
+def download_video(url, resolution, output_path, socketio=None):
     def progress_hook(d):
         if d['status'] == 'downloading':
             percent_str = strip_ansi_escape_sequences(d['_percent_str'])
@@ -30,18 +25,19 @@ def download_video(url, resolution, socketio=None):
         elif d['status'] == 'finished':
             socketio.emit('progress', {'percent': 100})
             socketio.emit('status', {'message': 'Download completed!'})
-            filename = d['info_dict']['_filename']
-            socketio.emit('download_link', {'link': f'/download/{os.path.basename(filename)}'})
 
-    download_dir = get_downloads_folder()
     ydl_opts = {
         'format': str(resolution),
-        'outtmpl': f'{download_dir}/%(title)s.%(ext)s',
+        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
         'progress_hooks': [progress_hook],
     }
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
+            # Get the file path of the downloaded video
+            info = ydl.extract_info(url, download=False)
+            video_file = ydl.prepare_filename(info)
+            socketio.emit('download_link', {'link': video_file})
     except yt_dlp.utils.DownloadError as e:
         socketio.emit('error', {'message': str(e)})
 
@@ -53,28 +49,26 @@ def index():
 def video_info():
     url = request.json['url']
     ydl_opts = {'quiet': True}
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-        thumbnail = info.get('thumbnail')
-        title = info.get('title')
-        formats = [
-            {'format_id': f['format_id'], 'format_note': f['format_note'] or f['height'], 'ext': f['ext']}
-            for f in info['formats'] if f.get('vcodec') != 'none' and f.get('acodec') != 'none'
-        ]
-        return jsonify({'thumbnail': thumbnail, 'title': title, 'formats': formats})
-
-@app.route('/download/<filename>', methods=['GET'])
-def download_file(filename):
-    download_dir = get_downloads_folder()
-    return send_from_directory(download_dir, filename, as_attachment=True)
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            thumbnail = info.get('thumbnail')
+            title = info.get('title')
+            formats = [
+                {'format_id': f['format_id'], 'format_note': f['format_note'] or f['height'], 'ext': f['ext']}
+                for f in info['formats'] if f.get('vcodec') != 'none' and f.get('acodec') != 'none'
+            ]
+            return jsonify({'thumbnail': thumbnail, 'title': title, 'formats': formats})
+    except yt_dlp.utils.DownloadError as e:
+        return jsonify({'error': str(e)})
 
 @socketio.on('download')
 def handle_download(data):
-    video_url = data['url']
+    url = data['url']
     resolution = data['resolution']
-    thread = threading.Thread(target=download_video, args=(video_url, resolution, socketio))
-    thread.start()
+    output_path = str(Path.home() / "Downloads")
+    download_thread = threading.Thread(target=download_video, args=(url, resolution, output_path, socketio))
+    download_thread.start()
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
